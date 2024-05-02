17848715b4c0a5441142add4f7a521dffc9e138a
graysari <165754890+graysari@users.noreply.github.com>
2024-05-02 09:41:30 +0000

    nikz

---
+++ Main.java
@@ -0,0 +1,72 @@
+public class Main{
+
+    public static void displayDetails(){
+                
+        Car dCar = new Car("Toyota","Vios",2030,11);
+
+        System.out.println("Car Details:");
+        System.out.println("Make: "+dCar.getMake());
+        System.out.println("Model: "+dCar.getModel());
+        System.out.println("Year: "+dCar.getYear());
+        System.out.println("Number of Doors: "+dCar.getNumberOfDoors());
+        
+    }
+    public static void main(String[] args) {
+
+        displayDetails();
+       }
+}
+class Vehicle{
+    private String make;
+    private String model;
+    private int year;
+
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
+
+class Car extends Vehicle{
+
+    private int numberOfDoors;
+
+    public Car(String make, String model,int year, int numberOfDoors){
+        setMake(make);
+        setModel(model);
+        setYear(year);
+        setNumberOfDoors(numberOfDoors);
+    }
+
+    public int getNumberOfDoors() {
+        return this.numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+
+
+}
